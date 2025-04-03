7244fb274d28744648b27aa9bd2fdf3951f49f33
Ivan Cordoba <119004564+ivan777cordoba@users.noreply.github.com>
2025-04-02 19:14:08 -0500

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal
+  {public static void main (string[] args)
+   {
+     new Principal()
+       }
+   Principal()
+  }
+ClaseHija claseHija = new ClaseHija()
+ClaseHija.setAtributo
