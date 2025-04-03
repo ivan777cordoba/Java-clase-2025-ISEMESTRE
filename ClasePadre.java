91a64824159448ba925d86bbc982d3aa06182b18
Ivan Cordoba <119004564+ivan777cordoba@users.noreply.github.com>
2025-04-02 19:09:44 -0500

    Create ClasePadre.java

---
+++ ClasePadre.java
@@ -0,0 +1,13 @@
+public class ClasePadre
+    {
+    private String Atributo1;
+    
+    private void setAtributo1(String param)
+        {
+        atributo1 = param;
+        }
+    public String getAtributo1()
+        {
+        return Atributo1;
+        }
+    }
